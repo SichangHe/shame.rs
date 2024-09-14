308560b5b885c6fa43efe2d0d9b398f6797efb84
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-14 11:08:21 -0700

    feat: add regex&thiserror

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,10 @@ pub use pub_fields;
 pub mod clone_on_write;
 
 pub use anyhow;
+pub use thiserror;
 pub use tracing;
 pub use tracing_subscriber;
+pub use regex;
 use tracing_subscriber::EnvFilter;
 
 pub mod prelude;
--- src/prelude.rs
+++ src/prelude.rs
@@ -5,6 +5,8 @@ pub use clone_on_write::CW;
 pub use derive_everything::*;
 pub use derive_where::derive_where;
 pub use pub_fields::pub_fields;
+pub use regex::{bytes::Regex as BytesRegex, Regex};
+pub use thiserror::Error;
 pub use tracing::{debug, error, info, instrument, trace, warn};
 
 pub fn init_tracing() {
